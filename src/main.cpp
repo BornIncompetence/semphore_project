d90b80694cf5c8de83323398bf7d907175990b8a
BornIncompetence <cagbrothers@gmail.com>
2019-05-04 21:11:03 -0700

    Now swaps actual contents of arrays

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <random>
 #include <semaphore.hpp>
 #include <sstream>
+#include <string.h>
+#include <string_view>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
@@ -10,20 +12,22 @@
 #include <sys/wait.h>
 
 constexpr auto CHUNK = 512;
+constexpr auto CHILDREN = 5;
+constexpr auto GROUPS = 4;
 
 struct group {
-  std::array<unsigned char, CHUNK> a;
-  std::array<unsigned char, CHUNK> b;
-  std::array<unsigned char, CHUNK> c;
+  std::array<char, CHUNK> a;
+  std::array<char, CHUNK> b;
+  std::array<char, CHUNK> c;
 
-  std::array<unsigned char, CHUNK> &operator[](int i) {
+  char *at(int i) {
     switch (i) {
     case 0:
-      return a;
+      return a.data();
     case 1:
-      return b;
+      return b.data();
     case 2:
-      return c;
+      return c.data();
     default:
       throw std::out_of_range("Attempted to go out of bounds!");
     }
@@ -44,7 +48,6 @@ std::pair<uint32_t, uint32_t> random_groups(std::mt19937 &rng);
 
 int main() {
   shared_memory mem_spaces{};
-  group g{};
 
   // Initialize shared memory buffers
   std::mt19937 rng{std::random_device{}()};
@@ -52,20 +55,18 @@ int main() {
   std::uniform_int_distribution upper_dist{'A', 'Z'};
   auto init_char = [&](auto &&arr, auto &&dist) {
     for (auto i = 0; i < CHUNK; ++i) {
-      arr[i] = static_cast<unsigned char>(dist(rng));
+      arr[i] = static_cast<char>(dist(rng));
     }
   };
 
   // Initialize semaphore here
-  SEMAPHORE s(4);
-  s.V(PUT_ITEM);
-  s.V(PUT_ITEM);
-  s.V(PUT_ITEM);
-  s.V(PUT_ITEM);
-  s.V(PUT_ITEM);
+  SEMAPHORE s(CHILDREN);
+  for (auto _ = 0; _ < CHILDREN; ++_) {
+    s.V(PUT_ITEM);
+  }
 
   // Initialize shared memory locations
-  for (const auto i : {0, 1, 2, 3}) {
+  for (auto i = 0; i < GROUPS; ++i) {
     const auto id = shmget(IPC_PRIVATE, sizeof(group), PERMS);
     mem_spaces.ids[i] = id;
     mem_spaces.buffers[i] = static_cast<group *>(shmat(id, nullptr, SHM_RND));
@@ -85,8 +86,19 @@ int main() {
   init_char(mem_spaces.buffers[3]->b, upper_dist);
   init_char(mem_spaces.buffers[3]->c, upper_dist);
 
+  // Print out contents of shared memory
+  for (auto i = 0; i < GROUPS; ++i) {
+    std::cout << '[' << i << "][" << 0 << "]: "
+              << std::string_view(mem_spaces.buffers[i]->a.data(), CHUNK)
+              << "\n[" << i << "][" << 1 << "]: "
+              << std::string_view(mem_spaces.buffers[i]->b.data(), CHUNK)
+              << "\n[" << i << "][" << 2 << "]: "
+              << std::string_view(mem_spaces.buffers[i]->c.data(), CHUNK)
+              << "\n\n";
+  }
+
   // Prompt user for number of operations to be performed
-  std::cout << "Enter the number of swaps you want each process to perform. ";
+  std::cout << "Enter the number of swaps you want each process to perform. \n";
   int swaps = 1;
   /*[]() {
     std::string line;
@@ -94,9 +106,9 @@ int main() {
     return std::stoi(line);
   }()*/
 
-  // Create 5 children
+  // Attempt to create 5 children (the program will continue if a child fails)
   std::vector<int> children{};
-  for (auto i = 0; i < 5; ++i) {
+  for (auto i = 0; i < CHILDREN; ++i) {
     const auto pid = fork();
     if (pid > 0) {
       children.push_back(pid);
@@ -120,9 +132,20 @@ int main() {
     return str;
   }();
 
-  // Wait for all five children to finish
+  // Wait for all children to finish
   for (auto &&_ : children) {
     wait(nullptr);
+  }
+
+  // Print out contents of shared memory
+  for (auto i = 0; i < GROUPS; ++i) {
+    std::cout << '[' << i << "][" << 0 << "]: "
+              << std::string_view(mem_spaces.buffers[i]->a.data(), CHUNK)
+              << "\n[" << i << "][" << 1 << "]: "
+              << std::string_view(mem_spaces.buffers[i]->b.data(), CHUNK)
+              << "\n[" << i << "][" << 2 << "]: "
+              << std::string_view(mem_spaces.buffers[i]->c.data(), CHUNK)
+              << "\n\n";
   }
 
   // Deallocate memory sections
@@ -133,6 +156,13 @@ int main() {
   }
 }
 
+void swap(char *arr1, char *arr2) {
+  char temp[512];
+  strncpy(temp, arr1, CHUNK);
+  strncpy(arr1, arr2, CHUNK);
+  strncpy(arr2, temp, CHUNK);
+}
+
 void child_process(SEMAPHORE &s, shared_memory &memory, int swaps) {
   std::mt19937 rng{std::random_device{}()};
 
@@ -141,7 +171,14 @@ void child_process(SEMAPHORE &s, shared_memory &memory, int swaps) {
   const auto [group1, group2] = random_groups(rng);
   for (auto i = 0; i < swaps; ++i) {
     s.P(PUT_ITEM);
-    std::swap(memory.buffers[group1][chunk1], memory.buffers[group2][chunk2]);
+    auto buf1 = memory.buffers[group1]->at(chunk1);
+    auto buf2 = memory.buffers[group2]->at(chunk2);
+    std::cout << '[' << getpid() << "]: Swapping memory at location: ["
+              << group1 << "][" << chunk1 << "] (" << static_cast<void *>(buf1)
+              << ") with [" << group2 << "][" << chunk2 << "] ("
+              << static_cast<void *>(buf2) << ")\n";
+    swap(memory.buffers[group1]->at(chunk1),
+         memory.buffers[group2]->at(chunk2));
     s.V(TAKE_ITEM);
   }
 }
