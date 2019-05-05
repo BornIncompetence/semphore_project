d58a40bd8118b8f21c9d782c8e6a6c1d4fa2b455
BornIncompetence <cagbrothers@gmail.com>
2019-05-04 18:52:30 -0700

    Now swaps memory in group

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,22 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-constexpr auto chunk = 512;
+constexpr auto CHUNK = 512;
 
 struct group {
-  unsigned char a[chunk];
-  unsigned char b[chunk];
-  unsigned char c[chunk];
+  unsigned char a[CHUNK];
+  unsigned char b[CHUNK];
+  unsigned char c[CHUNK];
 };
 
 struct shared_memory {
   std::array<int, 4> ids{};
-  std::array<char *, 4> buffers{};
+  std::array<group *, 4> buffers{};
 };
 
 enum { PUT_ITEM, TAKE_ITEM };
 
+void swap(unsigned char *, unsigned char *, int);
 void child_process(SEMAPHORE &, shared_memory &, int);
 void slowdown(std::mt19937 &);
 std::pair<uint32_t, uint32_t> random_chunks(std::mt19937 &rng);
@@ -39,11 +40,12 @@ int main() {
   std::uniform_int_distribution lower_dist{'a', 'z'};
   std::uniform_int_distribution upper_dist{'A', 'Z'};
   auto init_char = [&](auto &&arr, auto &&dist) {
-    for (auto i = 0; i < chunk; ++i) {
-      arr[i] = static_cast<char>(dist(rng));
+    for (auto i = 0; i < CHUNK; ++i) {
+      arr[i] = static_cast<unsigned char>(dist(rng));
     }
   };
 
+  // Initialize semaphore here
   SEMAPHORE s(4);
   s.V(PUT_ITEM);
   s.V(PUT_ITEM);
@@ -51,18 +53,26 @@ int main() {
   s.V(PUT_ITEM);
   s.V(PUT_ITEM);
 
-  // Test example of initialization
-  init_char(g.a, lower_dist);
-  init_char(g.b, upper_dist);
-  init_char(g.c, upper_dist);
-
+  // Initialize shared memory locations
   for (const auto i : {0, 1, 2, 3}) {
-    const auto size = sizeof(group);
-    std::cout << "Size of group: " << size << '\n';
-    const auto id = shmget(IPC_PRIVATE, size, PERMS);
+    const auto id = shmget(IPC_PRIVATE, sizeof(group), PERMS);
     mem_spaces.ids[i] = id;
-    mem_spaces.buffers[i] = static_cast<char *>(shmat(id, nullptr, SHM_RND));
+    mem_spaces.buffers[i] = static_cast<group *>(shmat(id, nullptr, SHM_RND));
   }
+
+  // Initialize shared memory locations with random letters
+  init_char(mem_spaces.buffers[0]->a, lower_dist);
+  init_char(mem_spaces.buffers[0]->b, lower_dist);
+  init_char(mem_spaces.buffers[0]->c, lower_dist);
+  init_char(mem_spaces.buffers[1]->a, upper_dist);
+  init_char(mem_spaces.buffers[1]->b, upper_dist);
+  init_char(mem_spaces.buffers[1]->c, upper_dist);
+  init_char(mem_spaces.buffers[2]->a, upper_dist);
+  init_char(mem_spaces.buffers[2]->b, upper_dist);
+  init_char(mem_spaces.buffers[2]->c, upper_dist);
+  init_char(mem_spaces.buffers[3]->a, upper_dist);
+  init_char(mem_spaces.buffers[3]->b, upper_dist);
+  init_char(mem_spaces.buffers[3]->c, upper_dist);
 
   // Prompt user for number of operations to be performed
   std::cout << "Enter the number of swaps you want each process to perform. ";
@@ -112,6 +122,14 @@ int main() {
   }
 }
 
+void swap(unsigned char *a, unsigned char *b, int n) {
+  for (auto i = 0; i < n; ++i) {
+    const auto temp = a[i];
+    b[i] = a[i];
+    a[i] = temp;
+  }
+}
+
 void child_process(SEMAPHORE &s, shared_memory &memory, int swaps) {
   std::mt19937 rng{std::random_device{}()};
 
@@ -120,27 +138,35 @@ void child_process(SEMAPHORE &s, shared_memory &memory, int swaps) {
   const auto [group1, group2] = random_groups(rng);
   for (auto i = 0; i < swaps; ++i) {
     s.P(PUT_ITEM);
-    std::cout << '[' << getpid() << "]: Mem Location 1: "
-              << static_cast<void *>(memory.buffers[group1] + 512 * chunk1)
-              << " Group " << group1 << " Chunk " << chunk1 << '\n';
-    std::cout << '[' << getpid() << "]: Mem Location 2: "
-              << static_cast<void *>(memory.buffers[group2] + 512 * chunk2)
-              << " Group " << group2 << " Chunk " << chunk2 << '\n';
+    const auto f = [&](uint32_t g, uint32_t c) -> unsigned char * {
+      switch (c) {
+      case 0:
+        return memory.buffers[g]->a;
+      case 1:
+        return memory.buffers[g]->b;
+      default:
+        return memory.buffers[g]->c;
+      }
+    };
+    swap(f(chunk1, group1), f(chunk2, group2), CHUNK);
     s.V(TAKE_ITEM);
   }
 }
 
+// Slowdown the execution of the child process
 void slowdown(std::mt19937 &rng) {
   std::uniform_int_distribution<uint32_t> u32dist{};
   while (u32dist(rng) >= 5000)
     ;
 }
 
+// Get two random numbers between 0 and 2
 std::pair<uint32_t, uint32_t> random_chunks(std::mt19937 &rng) {
   std::uniform_int_distribution dist{0, 2};
   return {dist(rng), dist(rng)};
 }
 
+// Get two random numbers between 0 and 3
 std::pair<uint32_t, uint32_t> random_groups(std::mt19937 &rng) {
   std::uniform_int_distribution dist{0, 3};
   return {dist(rng), dist(rng)};
